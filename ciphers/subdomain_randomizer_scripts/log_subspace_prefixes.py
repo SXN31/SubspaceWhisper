c604151db7dc2839bbeb47b970f786cb71a1d198
Stember, Jonathan <jstember@qualcomm.com>
2019-05-08 13:52:11 -0700

    added subspace. removed unneeded files

---
+++ ciphers/subdomain_randomizer_scripts/log_subspace_prefixes.py
@@ -0,0 +1,45 @@
+8#!/usr/bin/python
+# 
+# Filename:  log_subspace_prefixes.py
+#
+# Version: 1.0.0
+#
+# Author:  Professor James Moriarty
+#
+# Summary:  Appends or removes random 8-digit string plus ".communications.starfleet.federation.subspace" 
+# to each line of a file that's been Cloakified using log_subspace_prefixes cipher.
+#
+# Description:  
+#
+# Random Strings Generated from https://www.random.org/strings/?num=66&len=8&digits=on&upperalpha=on&loweralpha=on&unique=on&format=html&rnd=new
+
+import os, sys, getopt, random
+
+gCharList = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+if ( len(sys.argv) != 2 ):
+
+	print "usage: subspace_prefixes.py <cloakedFilename>"
+	print
+	exit
+
+else:
+
+    with open( sys.argv[1], "r" ) as file:
+
+            cloakedFile = file.read().splitlines()
+
+    with open( sys.argv[1], "w" ) as file:
+
+        for i in cloakedFile:
+
+            count = 0
+            subdomainNoise = ""
+
+            while ( count < 7 ):
+                subdomainNoise = subdomainNoise + gCharList[ (random.randint(0,35)) ]
+                count = count + 1
+
+            subdomainNoise = subdomainNoise + ".communications.starfleet.federation.subspace"
+
+            file.write( i + subdomainNoise + "\n" )
